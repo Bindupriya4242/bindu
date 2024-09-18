446c64de39168e5bb6b742b3fffee7f8abae1be3
Bindupriya4242 <bindupriya4242@gmail.com>
2024-09-18 19:20:18 -0400

    test

--- test.py
+++ test.py
@@ -1,11 +1,2 @@
 import pandas as pd
 
-# Example DataFrame
-data = {'A': [1, 2, 3, 4], 'B': [4, 3, 2, 1], 'C': [5, 6, 7, 8]}
-index = ['D1', 'D2', 'A1', 'B1']
-df = pd.DataFrame(data, index=index)
-
-# Select rows where index starts with 'D' and rank columns A, B, C
-ranked_df = df.loc[df.index.str.startswith('D'), ['A', 'B', 'C']].rank()
-
-print(ranked_df)
